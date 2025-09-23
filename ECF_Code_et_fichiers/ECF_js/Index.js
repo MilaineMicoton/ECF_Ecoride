1757f3aa62ab4a2e7432072deed018c3fa53553b
Milaine <marieheleneknight46@gmail.com>
2025-09-23 22:13:06 +0200

    version 1ère itération

--- ECF_Code_et_fichiers/ECF_js/Index.js
+++ ECF_Code_et_fichiers/ECF_js/Index.js
@@ -1,5 +1,9 @@
 // date du trajet, spécifier une date par défaut
 const dateControl = document.querySelector('input[type="date"]');
-dateControl.value = "2017-06-01";
-console.log(dateControl.value); // prints "2017-06-01"
-console.log(dateControl.valueAsNumber); // prints 1496275200000, a JavaScript timestamp (ms)
+dateControl.value = "2025-07-01";
+console.log(dateControl.value); // prints "2025-07-01"
+console.log(dateControl.valueAsNumber); // prints 1751328000000, a JavaScript timestamp (ms)
+// date d'aujourd'hui
+const date = new Date ();
+
+console.log(date.toString());
